66fdbbf96db6cb89cb5b6958ea2dfcb20d405190
Berkay Kulak <beko_468@hotmail.com>
2021-05-11 20:34:15 +0300

    11-05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { MainNavComponent } from './nav/main-nav/main-nav.component';
+import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent,
+    MainLayoutComponent,
+    AdminLayoutComponent,
+    MainNavComponent,
+    AdminNavComponent
   ],
   imports: [
     BrowserModule,
